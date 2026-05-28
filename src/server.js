3a14c6b3285f5dbb663635f0cda143784aecb11c
poomichopoom-p <Poomicho.p@gmail.com>
2026-05-29 00:27:35 +0700

    starter2

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { cors } from "cors";
+import helmet from "helmet";
+
+import { router as apiRouter } from "./roues/index.js";
+import cookieParser from "cookie-parser";
+
+const corsOption = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+  ],
+};
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Server is error!",
+    path: req.originalUrl,
+    method: req.method,
+    timestamp: new Data().toISOString(),
+    stack: err.stack,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Kinetix");
+});
+
+app.use("/api", apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running or Port: ${PORT} !!`);
+});
