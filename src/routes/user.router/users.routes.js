6feb7c2cd4bb383a2f18df82034019d237d5a18e
poomichopoom-p <Poomicho.p@gmail.com>
2026-06-03 21:21:13 +0700

    edit-user-router_rebase

--- src/routes/user.router/users.routes.js
+++ src/routes/user.router/users.routes.js
@@ -3,28 +3,20 @@ import authUser from "../../middelware/authUser.js";
 import {
   deleteUserById,
   getUserById,
+  login,
+  registerUser,
   updateUserById,
-} from "./user.controller.js";
+} from "../../modules/controller/user.controller.js";
 
 export const router = Router();
-
-
 
 router.post("/register", registerUser);
 
-router.post("/login",  login);
-
-
-
-export const router = Router();
+router.post("/login", login);
 
 router.get("/:id", authUser, getUserById);
 router.patch("/:id", authUser, updateUserById);
 router.delete("/:id", authUser, deleteUserById);
 
-
 router.post("/register", registerUser);
 router.post("/login", login);
-
-
-
