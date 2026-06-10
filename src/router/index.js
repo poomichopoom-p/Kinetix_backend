49e734f4bea548852b0532307a138ec7185a2d81
poomichopoom-p <Poomicho.p@gmail.com>
2026-06-10 23:16:13 +0700

    mergeFormReto

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import { router as staffRouter } from "./staff.router/staff.router.js";
 import { orderRouter as orderRouter } from "./order.router/order.router.js";
 import { router as brandRouter } from "./brand.router/brand.router.js";
 import { router as shoeRouter } from "./shoe.router/shoe.router.js";
-<<<<<<< HEAD
 import { router as categoryRouter } from "./category.router/category.router.js"; // New import
 import { router as deliveryAuthRouter } from "./deliveryAuth.router/deliveryAuth.router.js";
 import { router as jobRouter } from "./job.router/job.router.js";
@@ -16,14 +15,6 @@ import authUser from "../middleware/authUser.js";
 import isAdmin from "../middleware/isAdmin.js";
 import { adminRouter } from "./admin.router/admin.router.js";
 import { getUserRewards, redeemPoints } from "../modules/controller/user.controller.js";
-=======
-import { router as categoryRouter } from "./category.router/category.router.js"
-//import { router as jobRouter } from "./job.router/job.router.js";
-import { router as cartRouter } from "./cart.router/cart.router.js"
-import authUser from "../middleware/authUser.js";
-import isAdmin from "../middleware/isAdmin.js";
-import { adminRouter } from "./admin.router/admin.router.js";
->>>>>>> 1a91f3a1719f142fe56c895ac92eb143bd0e890a
 
 
 export const router = Router();
@@ -33,18 +24,10 @@ router.use("/users", usersRouter);
 router.use("/staff", staffRouter);
 router.use("/products", productsRouter);
 router.use("/products/admin", authUser, isAdmin, productsRouter);
-<<<<<<< HEAD
 router.use("/orders", authUser, isAdmin, orderRouter);
 router.use("/shoes", shoeRouter);
 router.use("/brands", brandRouter); // New route for brands
 router.use("/categories", categoryRouter); // New route for categories
-=======
-router.use("/order", authUser, /*isAdmin,*/ orderRouter);
-router.use("/order", authUser, isAdmin, orderRouter);
-router.use("/shoes", shoeRouter);
-router.use("/brands", brandRouter);
-router.use("/categories", categoryRouter);
->>>>>>> 1a91f3a1719f142fe56c895ac92eb143bd0e890a
 router.use("/cart", authUser, cartRouter);
 router.use("/admin", adminRouter);
 
